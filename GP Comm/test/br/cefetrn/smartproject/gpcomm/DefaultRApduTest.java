f99f514d534c93d5f1299b030da8d5c5e0af382c
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-27 16:30:27 +0000

    New test class.

---
+++ GP Comm/test/br/cefetrn/smartproject/gpcomm/DefaultRApduTest.java
@@ -0,0 +1,46 @@
+package br.cefetrn.smartproject.gpcomm;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Crístian Deives <cristiandeives@gmail.com>
+ */
+public class DefaultRApduTest {
+    private DefaultRApdu apdu;
+    
+    @Before
+    public void init() {
+        apdu = new DefaultRApdu();
+    }
+    
+    @Test
+    public void defaultValues() {
+        Assert.assertEquals((short) 0x9000, apdu.getSw());
+        Assert.assertNull(apdu.getData());
+    }
+    
+    @Test
+    public void statusWordBytes() {
+        Assert.assertEquals((byte) 0x90, apdu.getSw1());
+        Assert.assertEquals((byte) 0x00, apdu.getSw2());
+        apdu.setSw((short) 0x1234);
+        Assert.assertEquals((byte) 0x12, apdu.getSw1());
+        Assert.assertEquals((byte) 0x34, apdu.getSw2());
+        apdu.setSw((short) 0x0000);
+        Assert.assertEquals((byte) 0x00, apdu.getSw1());
+        Assert.assertEquals((byte) 0x00, apdu.getSw2());
+    }
+    
+    @Test
+    public void settersAndGetters() {
+        short my_sw = (short) 0x1234;
+        apdu.setSw(my_sw);
+        Assert.assertEquals(my_sw, apdu.getSw());
+        byte[] my_data = new byte[] {(byte) 0x01, (byte) 0x02, (byte) 0x03,
+                (byte) 0x04};
+        apdu.setData(my_data);
+        Assert.assertEquals(my_data, apdu.getData());
+    }
+}
