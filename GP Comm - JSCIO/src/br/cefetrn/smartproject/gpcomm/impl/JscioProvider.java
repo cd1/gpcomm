8e9bbfd0a84681bd66f0392f95811cf91178f156
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-27 16:37:41 +0000

    A stub for the Java Smart Card I/O implementation of GP Comm.

---
+++ GP Comm - JSCIO/src/br/cefetrn/smartproject/gpcomm/impl/JscioProvider.java
@@ -0,0 +1,9 @@
+package br.cefetrn.smartproject.gpcomm.impl;
+
+import br.cefetrn.smartproject.gpcomm.GpCommProvider;
+
+/**
+ * @author Crístian Deives <cristiandeives@gmail.com>
+ */
+public class JscioProvider implements GpCommProvider {
+}
