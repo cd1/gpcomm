5a4284be627cb3a6f8a3cb9018a8ba03bfa7ec4f
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-28 15:54:26 +0000

    execute method.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
@@ -5,6 +5,12 @@ package br.cefetrn.smartproject.gpcomm;
  */
 public class CDelete extends DefaultCApdu {
     public CDelete(byte[] aid) {
-        
+        // don't remember the bytes
+    }
+    
+    public RApdu execute(GpCommCard card) throws GpCommException {
+        RApdu response = super.execute(card);
+        RDelete response_delete = new RDelete(response);
+        return response_delete;
     }
 }
