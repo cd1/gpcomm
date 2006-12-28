d21eb6d8276c32726a99af77d81ac0d9600b9962
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-28 15:57:18 +0000

    Now this class extends Properties, and not HashMap.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommProperties.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommProperties.java
@@ -1,14 +1,14 @@
 package br.cefetrn.smartproject.gpcomm;
 
-import java.util.HashMap;
+import java.util.Properties;
 
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
-public class GpCommProperties extends HashMap<String, String> {
+public class GpCommProperties extends Properties {
     public static final String PROVIDER = "gpcomm.provider";
     
     public void setProvider(Class<? extends GpCommProvider> provider) {
-        put(PROVIDER, provider.getClass().getName());
+        setProperty(PROVIDER, provider.getClass().getName());
     }
 }
