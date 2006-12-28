089f51b84e861b2f63425b5f48f07d533bcbcf87
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-28 16:00:00 +0000

    Now one can create a new instance of this class (instead of getting it from a factory) and set the properties before it loads the provider class.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpComm.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/GpComm.java
@@ -2,52 +2,31 @@ package br.cefetrn.smartproject.gpcomm;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class GpComm {
-    private static GpComm instance;
     private GpCommProvider provider;
-    private Map<String, String> props;
+    private Properties properties;
     
-    private GpComm() {
+    public GpComm(Properties props) {
+        loadProperties(props);
+    }
+    
+    public GpComm() {
         Properties props_file = new Properties();
         InputStream props_file_stream = null;
         try {
             props_file_stream =
                     getClass().getResourceAsStream("gpcomm.properties");
             props_file.load(props_file_stream);
-            String provider_class_name =
-                    props_file.getProperty(GpCommProperties.PROVIDER);
-            if (provider_class_name == null) {
-                // couldn't read provider class property
-            }
-            else {
-                Class<? extends GpCommProvider> provider_class =
-                        (Class<? extends GpCommProvider>) Class.forName(
-                        provider_class_name);
-                provider = provider_class.newInstance();
-            }
+            loadProperties(props_file);
         }
         catch (IOException e) {
             // couldn't read gpcomm.properties
-        }
-        catch (ClassNotFoundException e) {
-            // couldn't find the provider class in the classpath
-        }
-        catch (ClassCastException e) {
-            // the provider class doesn't implement GpCommProvider
-        }
-        catch (IllegalAccessException e) {
-            // couldn't find the default constructor
-        }
-        catch (InstantiationException e) {
-            // the provider class couldn't be instantiated
         }
         finally {
             if (props_file_stream != null) {
@@ -61,30 +40,39 @@ public class GpComm {
         }
     }
     
-    public static GpComm getInstance() {
-        if (instance == null) {
-            instance = new GpComm();
+    private void loadProperties(Properties props) {
+        String provider_class_name =
+                props.getProperty(GpCommProperties.PROVIDER);
+        if (provider_class_name == null) {
+            // couldn't read provider class property
         }
-        return instance;
+        else {
+            try {
+                Class<? extends GpCommProvider> provider_class =
+                        (Class<? extends GpCommProvider>) Class.forName(
+                        provider_class_name);
+                provider = provider_class.newInstance();
+            }
+            catch (ClassNotFoundException e) {
+                // couldn't find the provider class in the classpath
+            }
+            catch (ClassCastException e) {
+                // the provider class doesn't implement GpCommProvider
+            }
+            catch (IllegalAccessException e) {
+                // couldn't find the default constructor
+            }
+            catch (InstantiationException e) {
+                // the provider class couldn't be instantiated
+            }
+        }
     }
     
-    public List<GpCommTerminal> getAvailableTerminals() {
-        return null;
+    public List<GpCommTerminal> getAvailableTerminals() throws GpCommException {
+        return provider.getAvailableTerminals();
     }
     
-    public String getVersion() {
-        return "GP Comm 0.1";
-    }
-    
-    public void setProperties(Map<String, String> props) {
-        this.props = props; // PENDING can't hold an external reference
-    }
-    
-    public void setProperty(String key, String value) {
-        props.put(key, value);
-    }
-    
-    public Map<String, String> getProperties() {
-        return Collections.unmodifiableMap(props);
+    public Properties getProperties() {
+        return (Properties) properties.clone();
     }
 }
