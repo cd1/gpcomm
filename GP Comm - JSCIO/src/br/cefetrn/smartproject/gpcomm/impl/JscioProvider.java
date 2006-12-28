f69843c9e7113020ab6b2c054de24462093aa5c7
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-28 15:19:44 +0000

    I started to implement this class.

--- GP Comm - JSCIO/src/br/cefetrn/smartproject/gpcomm/impl/JscioProvider.java
+++ GP Comm - JSCIO/src/br/cefetrn/smartproject/gpcomm/impl/JscioProvider.java
@@ -1,9 +1,37 @@
 package br.cefetrn.smartproject.gpcomm.impl;
 
+import br.cefetrn.smartproject.gpcomm.GpCommException;
 import br.cefetrn.smartproject.gpcomm.GpCommProvider;
+import br.cefetrn.smartproject.gpcomm.GpCommTerminal;
+import java.util.ArrayList;
+import java.util.List;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.TerminalFactory;
 
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class JscioProvider implements GpCommProvider {
+    private TerminalFactory terminalFactory;
+    
+    public JscioProvider() {
+        terminalFactory = TerminalFactory.getDefault();
+    }
+    
+    public List<GpCommTerminal> getAvailableTerminals() throws GpCommException {
+        try {
+            List<CardTerminal> jscio_terminals =
+                    terminalFactory.terminals().list();
+            List<GpCommTerminal> gpcomm_terminals =
+                    new ArrayList<GpCommTerminal>(jscio_terminals.size());
+            for (CardTerminal terminal : jscio_terminals) {
+                gpcomm_terminals.add(new GpCommTerminalImpl(terminal));
+            }
+            return gpcomm_terminals;
+        }
+        catch (CardException e) {
+            throw new GpCommException(e);
+        }
+    }
 }
