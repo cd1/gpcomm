a3bf10d8937a6d9d13566392c2dbedfdadf07e50
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-28 15:57:43 +0000

    Added two constructors.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommException.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommException.java
@@ -6,4 +6,11 @@ import java.io.IOException;
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class GpCommException extends IOException {
+    public GpCommException(String msg) {
+        super(msg);
+    }
+    
+    public GpCommException(Throwable cause) {
+        super(cause);
+    }
 }
