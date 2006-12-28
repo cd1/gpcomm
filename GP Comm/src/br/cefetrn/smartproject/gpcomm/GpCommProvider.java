7bc66b8d5584b25092c3f8acf9eebc083f61db72
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-28 15:56:38 +0000

    Wrote the method getAvailableTerminals.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommProvider.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommProvider.java
@@ -1,7 +1,10 @@
 package br.cefetrn.smartproject.gpcomm;
 
+import java.util.List;
+
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public interface GpCommProvider {
+    List<GpCommTerminal> getAvailableTerminals() throws GpCommException;
 }
