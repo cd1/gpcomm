c7e67aaaad97667886bdb284014d75013426eaf8
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-29 15:56:17 +0000

    Added the command bytes (although I don't think it's fully OK).

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
@@ -1,11 +1,24 @@
 package br.cefetrn.smartproject.gpcomm;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class CDelete extends DefaultCApdu {
     public CDelete(byte[] aid) {
-        // don't remember the bytes
+        setCla((byte) 0x80);
+        setIns((byte) 0xE4);
+        setP2((byte) 0x00);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write((byte) 0x4F);
+        baos.write((byte) aid.length);
+        try {
+            baos.write(aid);
+        }
+        catch (IOException e) {/* it'll necer catch */}
+        setData(baos.toByteArray());
     }
     
     public RApdu execute(GpCommCard card) throws GpCommException {
