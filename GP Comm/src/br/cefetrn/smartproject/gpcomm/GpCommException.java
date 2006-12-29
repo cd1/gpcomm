c09b8cb616f5bb74b1035616d1a2fb29a0131bce
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-29 20:49:36 +0000

    The exception now extends java.lang.Exception and I added a constructor with a message and another exception.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommException.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommException.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
-public class GpCommException extends IOException {
+public class GpCommException extends Exception {
     public GpCommException(String msg) {
         super(msg);
     }
     
     public GpCommException(Throwable cause) {
         super(cause);
+    }
+    
+    public GpCommException(String msg, Throwable cause) {
+        super(msg, cause);
     }
 }
