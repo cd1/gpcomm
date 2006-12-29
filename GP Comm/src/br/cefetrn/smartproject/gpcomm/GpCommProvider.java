1c6854af4b3bb539766f2e297765ab5ef63b247a
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-29 20:50:01 +0000

    Added the close method.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommProvider.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommProvider.java
@@ -7,4 +7,6 @@ import java.util.List;
  */
 public interface GpCommProvider {
     List<GpCommTerminal> getAvailableTerminals() throws GpCommException;
+
+    void close() throws GpCommException;
 }
