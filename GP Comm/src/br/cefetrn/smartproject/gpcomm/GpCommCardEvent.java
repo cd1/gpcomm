19533b352e5c54c99c7a93432efdeccf8c8d4740
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2006-12-29 15:53:11 +0000

    Handled the cloning of the Date object and put the GpCommCardEventType enum inside GpCommCardEvent.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommCardEvent.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommCardEvent.java
@@ -6,16 +6,18 @@ import java.util.Date;
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class GpCommCardEvent {
+    public enum Type {INSERTED, REMOVED};
+    
     private Date time;
     private GpCommCard card;
     
     public GpCommCardEvent(Date time, GpCommCard card) {
-        this.time = time;
+        this.time = (Date) time.clone();
         this.card = card;
     }
     
     public Date getTime() {
-        return time;
+        return (Date) time.clone();
     }
     
     public GpCommCard getCard() {
--- GP Comm/src/br/cefetrn/smartproject/gpcomm/GpCommCardEventType.java
+++
@@ -1,9 +0,0 @@
-package br.cefetrn.smartproject.gpcomm;
-
-/**
- * @author Crístian Deives <cristiandeives@gmail.com>
- */
-public enum GpCommCardEventType {
-    INSERTED,
-    REMOVED;
-}
