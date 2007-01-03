a0c152184669341218036542878480c656082d59
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-03 17:07:12 +0000

    Removed the execute method and added two methods to deal with the AID.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/CSelect.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/CSelect.java
@@ -4,18 +4,24 @@ package br.cefetrn.smartproject.gpcomm;
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class CSelect extends DefaultCApdu {
+    public CSelect() {
+        setIns((byte) 0xA4);
+        setP1((byte) 0x04);
+    }
+    
     public CSelect(byte[] aid) {
+        this();
+        setAid(aid);
+    }
+    
+    public byte[] getAid() {
+        return data;
+    }
+    
+    public void setAid(byte[] aid) {
         if (aid == null) {
             throw new NullPointerException("AID can't be null");
         }
-        setIns((byte) 0xA4);
-        setP1((byte) 0x04);
         setData(aid);
-    }
-    
-    public RApdu execute(GpCommCard card) throws GpCommException {
-        RApdu response = super.execute(card);
-        RSelect response_select = new RSelect(response);
-        return response_select;
     }
 }
