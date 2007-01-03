4c37f1b1251974265c57fa1993267b7e650d0773
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-03 17:06:35 +0000

    Removed the execute method and added two methods to deal with the AID.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
@@ -7,10 +7,22 @@ import java.io.IOException;
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class CDelete extends DefaultCApdu {
-    public CDelete(byte[] aid) {
+    public CDelete() {
         setCla((byte) 0x80);
         setIns((byte) 0xE4);
         setP2((byte) 0x00);
+    }
+    
+    public CDelete(byte[] aid) {
+        this();
+        setAid(aid);
+    }
+    
+    public byte[] getAid() {
+        return data;
+    }
+    
+    public void setAid(byte[] aid) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         baos.write((byte) 0x4F);
         baos.write((byte) aid.length);
@@ -19,11 +31,5 @@ public class CDelete extends DefaultCApdu {
         }
         catch (IOException e) {/* it'll necer catch */}
         setData(baos.toByteArray());
-    }
-    
-    public RApdu execute(GpCommCard card) throws GpCommException {
-        RApdu response = super.execute(card);
-        RDelete response_delete = new RDelete(response);
-        return response_delete;
     }
 }
