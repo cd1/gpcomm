e53ed27d10cfdb09bd112c958068b172e38ff7d4
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-03 17:12:03 +0000

    Added two append methods and now they use a StringBuilder instead of an Appendable.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/Util.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/Util.java
@@ -1,26 +1,28 @@
 package br.cefetrn.smartproject.gpcomm;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class Util {
-    private static final Logger log = Logger.getLogger(Util.class.getName());
-    
     public static String fromByteArrayToString(byte[] array) {
-        if (array == null || array.length == 0) {
-            return "null";
-        }
         StringBuilder sb = new StringBuilder();
-        for (byte b : array) {
-            appendByteAsString(b, sb);
-            sb.append(" ");
-        }
-        sb.deleteCharAt(sb.length() - 1); // deletes the last space
+        appendByteArrayAsString(array, sb);
         return sb.toString();
+    }
+    
+    public static void appendByteArrayAsString(byte[] array, StringBuilder sb) {
+        if (array == null) {
+            sb.append("null");
+        }
+        else {
+            for (byte b : array) {
+                appendByteAsString(b, sb);
+                sb.append(" ");
+            }
+            if (sb.length() > 0) { // all the cases, except when array = byte[0]
+                sb.deleteCharAt(sb.length() - 1); // deletes the last space
+            }
+        }
     }
     
     public static String fromByteToString(byte b) {
@@ -29,22 +31,22 @@ public class Util {
         return sb.toString();
     }
     
-    public static void appendByteAsString(byte b, Appendable appendable) {
-        try {
-            String tmp = Integer.toHexString(b & 0xFF).toUpperCase();
-            appendable.append("0x");
-            if (tmp.length() == 1) {
-                appendable.append("0");
-            }
-            appendable.append(tmp);
+    public static void appendByteAsString(byte b, StringBuilder sb) {
+        String tmp = Integer.toHexString(b & 0xFF).toUpperCase();
+        sb.append("0x");
+        if (tmp.length() == 1) {
+            sb.append("0");
         }
-        catch (IOException e) {
-            log.log(Level.WARNING, "Exception while appendind a String", e);
-        }
+        sb.append(tmp);
     }
     
     public static String fromShortToString(short s) {
         StringBuilder sb = new StringBuilder(6); // 0xXXXX
+        appendShortAsString(s, sb);
+        return sb.toString();
+    }
+    
+    public static void appendShortAsString(short s, StringBuilder sb) {
         sb.append("0x");
         String tmp = Integer.toHexString(s & 0xFFFF).toUpperCase();
         int n_zeros = 4 - tmp.length();
@@ -52,6 +54,5 @@ public class Util {
             sb.append("0");
         }
         sb.append(tmp);
-        return sb.toString();
     }
 }
