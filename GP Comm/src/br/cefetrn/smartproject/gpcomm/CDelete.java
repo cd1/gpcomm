0d97f9b82afd4e8133333762e328f822ec64e841
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-04 04:25:47 +0000

    Fixed the getAid method and verified for null on setAid.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
@@ -18,17 +18,25 @@ public class CDelete extends DefaultCApdu {
     }
     
     public byte[] getAid() {
-        return data;
+        if (data == null) {
+            return null;
+        }
+        byte[] new_array = new byte[data.length - 2];
+        System.arraycopy(data, 2, new_array, 0, new_array.length);
+        return new_array;
     }
     
     public void setAid(byte[] aid) {
+        if (aid == null) {
+            throw new NullPointerException("AID can't be null");
+        }
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         baos.write((byte) 0x4F);
         baos.write((byte) aid.length);
         try {
             baos.write(aid);
         }
-        catch (IOException e) {/* it'll necer catch */}
+        catch (IOException e) {/* it'll never catch */}
         setData(baos.toByteArray());
     }
 }
