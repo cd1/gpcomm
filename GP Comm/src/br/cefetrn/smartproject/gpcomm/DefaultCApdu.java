ea59ef75e99fcd8a0e2f25a6a19ce9b93a4b4036
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-05 16:37:05 +0000

    Changed the exception thrown in the constructor DefaultCApdu(byte[]) and added Javadoc tags.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/DefaultCApdu.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/DefaultCApdu.java
@@ -6,27 +6,51 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
+ * A default implementation of {@link CApdu}.
+ * 
  * @author Crístian Deives <cristiandeives@gmail.com>
+ * @see CApdu
  */
 public class DefaultCApdu implements CApdu {
+    /** CLA byte. */
     protected byte cla;
+    /** INS byte. */
     protected byte ins;
+    /** P1 byte. */
     protected byte p1;
+    /** P2 byte. */
     protected byte p2;
+    /** Data field. */
     protected byte[] data;
+    /** LE byte. */
     protected byte le;
 
     private static final Logger log =
             Logger.getLogger(DefaultCApdu.class.getName());
     
+    /**
+     * Creates a new command with all the bytes zeroed.
+     */
     public DefaultCApdu() {
         // default
     }
     
-    public DefaultCApdu(byte[] command) throws GpCommException {
+    /**
+     * Creates a new command with the specified bytes.
+     * 
+     * @param command A complete APDU command, in one of the 4 standard cases
+     * (1: no command data, no responde data; 2: no command data, with response
+     * data; 3: with command data, no response data; 4: with command data, with
+     * response data). There is no problem if there are extra bytes in this
+     * parameter, it'll just be logged.
+     * @throws IllegalArgumentException If the length of {@code command} is less
+     * than 4 or if there are less bytes in the data field than the value
+     * specified in the LC field.
+     */
+    public DefaultCApdu(byte[] command) {
         ByteArrayInputStream bais = new ByteArrayInputStream(command);
         if (bais.available() < 4) {
-            throw new GpCommException("Invalid command APDU length: " +
+            throw new IllegalArgumentException("Invalid command APDU length: " +
                     bais.available());
         }
         setCla((byte) bais.read());
@@ -43,8 +67,8 @@ public class DefaultCApdu implements CApdu {
                 int read = bais.read(data, 0, lc);
                 // case 3
                 if (read < lc) {
-                    throw new GpCommException("Invalid LC field: found " +
-                            read + " bytes but was expected " + lc);
+                    throw new IllegalArgumentException("Invalid LC field: " +
+                            "found " + read + " bytes but was expected " + lc);
                 }
                 else {
                     if (bais.available() > 0) { // case 4
@@ -70,6 +94,11 @@ public class DefaultCApdu implements CApdu {
         }
     }
     
+    /**
+     * Changes the class byte.
+     * 
+     * @param cla The CLA byte.
+     */
     public void setCla(byte cla) {
         this.cla = cla;
     }
@@ -78,6 +107,11 @@ public class DefaultCApdu implements CApdu {
         return cla;
     }
 
+    /**
+     * Changes the instruction byte.
+     * 
+     * @param ins The INS byte.
+     */
     public void setIns(byte ins) {
         this.ins = ins;
     }
@@ -86,6 +120,11 @@ public class DefaultCApdu implements CApdu {
         return ins;
     }
 
+    /**
+     * Changes the first parameter byte.
+     * 
+     * @param p1 The P1 byte.
+     */
     public void setP1(byte p1) {
         this.p1 = p1;
     }
@@ -94,6 +133,11 @@ public class DefaultCApdu implements CApdu {
         return p1;
     }
 
+    /**
+     * Changes the second parameter byte.
+     * 
+     * @param p2 The P2 byte.
+     */
     public void setP2(byte p2) {
         this.p2 = p2;
     }
@@ -102,10 +146,21 @@ public class DefaultCApdu implements CApdu {
         return p2;
     }
 
+    /**
+     * Returns the length of the data field.
+     * 
+     * @return The LC byte. This field is always calculated.
+     */
     public byte getLc() {
         return (byte) ((data == null) ? 0 : data.length);
     }
 
+    /**
+     * Changes the data field.
+     * 
+     * @param data The data field. If {@code data.length == 0}, then the data
+     * will be {@code null}.
+     */
     public void setData(byte[] data) {
         this.data = (data == null || data.length == 0)
                 ? null
@@ -116,6 +171,11 @@ public class DefaultCApdu implements CApdu {
         return (data == null) ? null : (byte[]) data.clone();
     }
 
+    /**
+     * Changes the number of bytes expected in the response APDU.
+     * 
+     * @param le The LE field.
+     */
     public void setLe(byte le) {
         this.le = le;
     }
@@ -143,6 +203,13 @@ public class DefaultCApdu implements CApdu {
         return baos.toByteArray();
     }
     
+    /**
+     * Returns a String that shows all the values of this command.
+     * 
+     * @return A String representation of this command.
+     * @see Util#fromByteToString
+     * @see Util#fromByteArrayToString
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("cla=");
