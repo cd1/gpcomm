74a893ef240c58f86e704e66c98e2634a75f1850
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-05 16:36:21 +0000

    Removed the default constructor (there must not be a command without an AID) and added Javadoc tags.

--- GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/CDelete.java
@@ -4,28 +4,40 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
+ * The GlobalPlatform DELETE command.
+ * 
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
 public class CDelete extends DefaultCApdu {
-    public CDelete() {
+    /**
+     * Creates a new object with a specified AID.
+     * 
+     * @param aid The AID of the application to be deleted.
+     * @throws NullPointerException if {@code aid} is {@code null}.
+     */
+    public CDelete(byte[] aid) {
         setCla((byte) 0x80);
         setIns((byte) 0xE4);
-    }
-    
-    public CDelete(byte[] aid) {
-        this();
         setAid(aid);
     }
     
+    /**
+     * Returns the AID of the application to be deleted.
+     * 
+     * @return An AID.
+     */
     public byte[] getAid() {
-        if (data == null) {
-            return null;
-        }
         byte[] new_array = new byte[data.length - 2];
         System.arraycopy(data, 2, new_array, 0, new_array.length);
         return new_array;
     }
     
+    /**
+     * Changes the AID of the application to be deleted.
+     * 
+     * @param aid The AID of the application to be deleted.
+     * @throws NullPointerException if {@code aid} is {@code null}.
+     */
     public void setAid(byte[] aid) {
         if (aid == null) {
             throw new NullPointerException("AID can't be null");
--- GP Comm/src/br/cefetrn/smartproject/gpcomm/CSelect.java
+++ GP Comm/src/br/cefetrn/smartproject/gpcomm/CSelect.java
@@ -1,23 +1,37 @@
 package br.cefetrn.smartproject.gpcomm;
 
 /**
+ * The GlobalPlatform SELECT command.
+ * 
  * @author Crístian Deives <cristiandeives@gmail.com>
  */
-public class CSelect extends DefaultCApdu {
-    public CSelect() {
+public class CSelect extends DefaultCApdu {    
+    /**
+     * Creates a new object with a specified AID.
+     * 
+     * @param aid The AID of the application to be selected.
+     * @throws NullPointerException If {@code aid} is {@code null}.
+     */
+    public CSelect(byte[] aid) {
         setIns((byte) 0xA4);
         setP1((byte) 0x04);
-    }
-    
-    public CSelect(byte[] aid) {
-        this();
         setAid(aid);
     }
     
+    /**
+     * Returns the AID of the application to be selected.
+     * 
+     * @return An AID.
+     */
     public byte[] getAid() {
         return data;
     }
     
+    /**
+     * Changes the AID of the application to be selected.
+     * 
+     * @param aid The AID of the application to be selected.
+     */
     public void setAid(byte[] aid) {
         if (aid == null) {
             throw new NullPointerException("AID can't be null");
