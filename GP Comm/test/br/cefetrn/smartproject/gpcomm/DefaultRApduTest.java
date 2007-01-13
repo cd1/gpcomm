3c56f8db9110173956923e45294f240c99e1aacc
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-13 14:34:10 +0000

    Fixed test because I changed the behavior of the Util class.

--- GP Comm/test/br/cefetrn/smartproject/gpcomm/DefaultRApduTest.java
+++ GP Comm/test/br/cefetrn/smartproject/gpcomm/DefaultRApduTest.java
@@ -1,5 +1,7 @@
 package br.cefetrn.smartproject.gpcomm;
 
+import br.cefetrn.smartproject.gpcomm.apdu.DefaultRApdu;
+import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,8 +20,8 @@ public class DefaultRApduTest {
     @Test
     public void defaultValues() {
         Assert.assertEquals((short) 0x9000, apdu.getSw());
-        Assert.assertNull(apdu.getData());
-        Assert.assertEquals("sw=0x9000,data=null", apdu.toString());
+        Assert.assertEquals(0, apdu.getData().length);
+        Assert.assertEquals("sw=9000,data=<empty>", apdu.toString());
     }
     
     @Test
@@ -29,11 +31,11 @@ public class DefaultRApduTest {
         apdu.setSw((short) 0x1234);
         Assert.assertEquals((byte) 0x12, apdu.getSw1());
         Assert.assertEquals((byte) 0x34, apdu.getSw2());
-        Assert.assertEquals("sw=0x1234,data=null", apdu.toString());
+        Assert.assertEquals("sw=1234,data=<empty>", apdu.toString());
         apdu.setSw((short) 0x0000);
         Assert.assertEquals((byte) 0x00, apdu.getSw1());
         Assert.assertEquals((byte) 0x00, apdu.getSw2());
-        Assert.assertEquals("sw=0x0000,data=null", apdu.toString());
+        Assert.assertEquals("sw=0000,data=<empty>", apdu.toString());
     }
     
     @Test
@@ -44,11 +46,8 @@ public class DefaultRApduTest {
         byte[] my_data = new byte[] {(byte) 0x01, (byte) 0x02, (byte) 0x03,
                 (byte) 0x04};
         apdu.setData(my_data);
-        // JUnit doesn't compare two byte[] as arrays!!!
-        for (int i = 0; i < my_data.length; i++) {
-            Assert.assertEquals(my_data[i], apdu.getData()[i]);
-        }
-        Assert.assertEquals("sw=0x1234,data=0x01 0x02 0x03 0x04",
+        Assert.assertTrue(Arrays.equals(my_data, apdu.getData()));
+        Assert.assertEquals("sw=1234,data=01 02 03 04",
                 apdu.toString());
     }
 }
--- GP Comm/test/br/cefetrn/smartproject/gpcomm/UtilTest.java
+++ GP Comm/test/br/cefetrn/smartproject/gpcomm/UtilTest.java
@@ -10,61 +10,63 @@ public class UtilTest {
     @Test
     public void byteToStringConversions() {
         byte b = (byte) 0x00;
-        Assert.assertEquals("0x00", Util.fromByteToString(b));
+        Assert.assertEquals("00", Util.fromByteToString(b));
         b = (byte) 0x01;
-        Assert.assertEquals("0x01", Util.fromByteToString(b));
+        Assert.assertEquals("01", Util.fromByteToString(b));
         b = (byte) 0x0A;
-        Assert.assertEquals("0x0A", Util.fromByteToString(b));
+        Assert.assertEquals("0A", Util.fromByteToString(b));
         b = (byte) 0x10;
-        Assert.assertEquals("0x10", Util.fromByteToString(b));
+        Assert.assertEquals("10", Util.fromByteToString(b));
         b = (byte) 0x64;
-        Assert.assertEquals("0x64", Util.fromByteToString(b));
+        Assert.assertEquals("64", Util.fromByteToString(b));
         b = (byte) 0x7F;
-        Assert.assertEquals("0x7F", Util.fromByteToString(b));
+        Assert.assertEquals("7F", Util.fromByteToString(b));
         b = (byte) 0xC8;
-        Assert.assertEquals("0xC8", Util.fromByteToString(b));
+        Assert.assertEquals("C8", Util.fromByteToString(b));
         b = (byte) 0xFF;
-        Assert.assertEquals("0xFF", Util.fromByteToString(b));
+        Assert.assertEquals("FF", Util.fromByteToString(b));
     }
     
     @Test
     public void byteArrayToStringConversions() {
         byte[] b = null;
         Assert.assertEquals("null", Util.fromByteArrayToString(b));
+        b = new byte[0];
+        Assert.assertEquals("<empty>", Util.fromByteArrayToString(b));
         b = new byte[] {(byte) 0x10};
-        Assert.assertEquals("0x10", Util.fromByteArrayToString(b));
+        Assert.assertEquals("10", Util.fromByteArrayToString(b));
         b = new byte[] {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03,
                 (byte) 0x04};
-        Assert.assertEquals("0x00 0x01 0x02 0x03 0x04",
+        Assert.assertEquals("00 01 02 03 04",
                 Util.fromByteArrayToString(b));
         b = new byte[] {(byte) 0x00, (byte) 0xA4, (byte) 0x04, (byte) 0x00,
                 (byte) 0x05, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04,
                 (byte) 0x05, (byte) 0x7F};
-        Assert.assertEquals("0x00 0xA4 0x04 0x00 0x05 0x01 0x02 0x03 0x04 " +
-                "0x05 0x7F", Util.fromByteArrayToString(b)); // SELECT
+        Assert.assertEquals("00 A4 04 00 05 01 02 03 04 05 7F",
+                Util.fromByteArrayToString(b)); // SELECT
     }
     
     @Test
     public void shortToStringConversions() {
         short s = (short) 0x0000;
-        Assert.assertEquals("0x0000", Util.fromShortToString(s));
+        Assert.assertEquals("0000", Util.fromShortToString(s));
         s = (short) 0x0001;
-        Assert.assertEquals("0x0001", Util.fromShortToString(s));
+        Assert.assertEquals("0001", Util.fromShortToString(s));
         s = (short) 0x000A;
-        Assert.assertEquals("0x000A", Util.fromShortToString(s));
+        Assert.assertEquals("000A", Util.fromShortToString(s));
         s = (short) 0x0010;
-        Assert.assertEquals("0x0010", Util.fromShortToString(s));
+        Assert.assertEquals("0010", Util.fromShortToString(s));
         s = (short) 0x0100;
-        Assert.assertEquals("0x0100", Util.fromShortToString(s));
+        Assert.assertEquals("0100", Util.fromShortToString(s));
         s = (short) 0x1000;
-        Assert.assertEquals("0x1000", Util.fromShortToString(s));
+        Assert.assertEquals("1000", Util.fromShortToString(s));
         s = (short) 0x9000;
-        Assert.assertEquals("0x9000", Util.fromShortToString(s));
+        Assert.assertEquals("9000", Util.fromShortToString(s));
         s = (short) 0x1234;
-        Assert.assertEquals("0x1234", Util.fromShortToString(s));
+        Assert.assertEquals("1234", Util.fromShortToString(s));
         s = (short) 0x7FFF;
-        Assert.assertEquals("0x7FFF", Util.fromShortToString(s));
+        Assert.assertEquals("7FFF", Util.fromShortToString(s));
         s = (short) 0xFFFF;
-        Assert.assertEquals("0xFFFF", Util.fromShortToString(s));
+        Assert.assertEquals("FFFF", Util.fromShortToString(s));
     }
 }
