01e2c5337627a0dea721947762ad6e4a442a0503
cristiandeives <cristiandeives@3d8da2e2-c424-0410-92e9-33dde47f2f63>
2007-01-13 14:42:22 +0000

    Fixed somed import statements.

--- GP Comm - JSCIO/src/br/cefetrn/smartproject/gpcomm/impl/GpCommCardImpl.java
+++ GP Comm - JSCIO/src/br/cefetrn/smartproject/gpcomm/impl/GpCommCardImpl.java
@@ -1,12 +1,12 @@
 package br.cefetrn.smartproject.gpcomm.impl;
 
-import br.cefetrn.smartproject.gpcomm.CApdu;
-import br.cefetrn.smartproject.gpcomm.CDelete;
-import br.cefetrn.smartproject.gpcomm.CSelect;
-import br.cefetrn.smartproject.gpcomm.DefaultRApdu;
 import br.cefetrn.smartproject.gpcomm.GpCommCard;
 import br.cefetrn.smartproject.gpcomm.GpCommException;
-import br.cefetrn.smartproject.gpcomm.RApdu;
+import br.cefetrn.smartproject.gpcomm.apdu.CApdu;
+import br.cefetrn.smartproject.gpcomm.apdu.CDelete;
+import br.cefetrn.smartproject.gpcomm.apdu.CSelect;
+import br.cefetrn.smartproject.gpcomm.apdu.DefaultRApdu;
+import br.cefetrn.smartproject.gpcomm.apdu.RApdu;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.smartcardio.Card;
@@ -14,6 +14,7 @@ import javax.smartcardio.CardChannel;
 import javax.smartcardio.CardException;
 import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
+
 
 /**
  * @author Crístian Deives <cristiandeives@gmail.com>
